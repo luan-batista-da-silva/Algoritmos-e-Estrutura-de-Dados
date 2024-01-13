c4a67130186872ed784fd8786f9765756724d4bc
Luan Batista da Silva <Luan Batista da Silva>
2024-01-13 11:43:13 -0300

    Funções de Adicionar na Cabeça e na Cauda da Lista

--- L05_Listas-Duplamentes-Encadeadas/list.c
+++ L05_Listas-Duplamentes-Encadeadas/list.c
@@ -15,3 +15,63 @@ typedef struct _doubly_linked_list {
     Node* end;
     size_t size;
 } DoublyLinkedList, List;
+
+List* List_create() {
+    List* L = (List*)calloc(1, sizeof(List));
+
+    L->begin = (Node*)NULL;
+    L->end = (Node*)NULL;
+    L->size = 0;
+
+    return L;
+}
+
+Node* Node_create(int val) {
+    Node* node = (Node*)calloc(1, sizeof(Node));
+
+    node->prev = (Node*)NULL;
+    node->next = (Node*)NULL;
+    node->val = val;
+
+    return node;
+}
+
+void List_destroy(List** L_ref) {
+    List* L = *L_ref;
+    Node* aux = (Node*)NULL;
+
+    for (Node* p = L->begin; p != NULL; p = p->prev) {
+        aux = p;
+        p = p->next;
+        free(aux);
+    }
+
+    free(L);
+    *L_ref = (List*)NULL;
+}
+
+bool List_is_empty(const List* L) {
+    return (L->begin == NULL && L->end == NULL && L->size == 0);
+}
+
+void List_add_first(List* L, int val) {
+    Node* p = Node_create(val);
+
+    if (List_is_empty(L)) {
+        L->end = p;
+    }
+    p->next = L->begin;
+    L->begin->prev = p;
+    L->begin = p;
+    L->size++;
+}
+
+
+void List_add_last(List* L, int val) {
+    Node* p = Node_create(val);
+
+    L->end->next = p;
+    p->prev = L->end;
+    L->end;
+    L->size++;
+}
--- L05_Listas-Duplamentes-Encadeadas/list.h
+++ L05_Listas-Duplamentes-Encadeadas/list.h
@@ -1,7 +1,17 @@
 #ifndef LIST_H
 #define LIST_H
 
+#include <stdbool.h>
+
 typedef struct _doubly_node DoublyNode, Node;
 typedef struct _doubly_linked_list DoublyLinkedList, List;
+
+List* List_create();
+Node* Node_create(int val);
+
+void List_destroy(List** L_ref);
+bool List_is_empty(const List* L);
+void List_add_first(List* L, int val);
+void List_add_last(List* L, int val);
 
 #endif
